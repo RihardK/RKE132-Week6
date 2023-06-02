c82fcab1aea00dc91682ea297cdd1eece62abde5
Rix <rihard.konnapere@tktk.ee>
2023-06-02 18:20:21 +0300

    Add week 6 code

---
+++ RndElem/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string[] snacks = {"sushi", "pizza", "burger", "kebab", "asian" };
+
+Random rnd = new Random();
+
+int rndIndex = rnd.Next(0, snacks.Length + 1);
+
+Console.WriteLine($"The food will be: {snacks[rndIndex]}");
---
+++ Week6_StringsAndArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string hello = "suspicious imposter.";
+hello = hello.Trim();
+
+int stringLength = hello.Length;
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+
+int wordCount = 1;
+
+for (int i = 0; i < hello.Length; i++)
+{
+    if (char.IsWhiteSpace(hello[i]))
+    {
+        wordCount++;
+    }
+}
+
+Console.WriteLine($"Word count in '{hello}' is : {wordCount} words");
